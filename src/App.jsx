1b859590154720d0a0b11c75723d00d3d03940bf
AfiyaZannuba <afiya.zannubaa@gmail.com>
2023-07-30 01:16:05 +0530

    I am creating css styling here by using module. This is pretty awesome since I don't have to use global css i.e, app.css targeted to every silly small components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import ContactForm from './components/ContactForm/ContactForm'
+import ContactHeader from './components/ContactHeader.jsx/ContactHeader'
+import Navigation from './components/Navigation/Navigation'
 
 const App = () => {
 
   return (
    <div>
-    
+    <Navigation />
+    <ContactHeader />
+    <ContactForm />
    </div>
   )
 }
